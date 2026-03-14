c3c9374f5150cb2d0e2cd2a018b3d54ddc5e1bd0
Stanislav Koshynskyi <stasony10@gmail.com>
2026-03-14 10:40:28 +0200

    money

--- src/main/java/entity/Money.java
+++ src/main/java/entity/Money.java
@@ -10,5 +10,19 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class Money {
     private BigDecimal money;
+    public Money(BigDecimal money) {
+        setMoney(money);
+    }
+    public void setMoney(BigDecimal money) {
+        if (money == null) {
+            throw new IllegalArgumentException("Money is null");
+        }
+        if (money.compareTo(BigDecimal.ZERO) > 0) {
+            this.money = money;
+        }
+        else{
+            throw new IllegalArgumentException("Money is negative");
+        }
+    }
 }
 
