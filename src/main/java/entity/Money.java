100231017d4914ee2be63b7e2270e91fd6a77db5
Stanislav Koshynskyi <stasony10@gmail.com>
2026-03-14 10:31:27 +0200

    feat: money class

---
+++ src/main/java/entity/Money.java
@@ -0,0 +1,14 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+@Getter
+@Setter
+@AllArgsConstructor
+public class Money {
+    private BigDecimal money;
+}
+
