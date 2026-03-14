e098da6d581c466402ff73df6833a870cf8e782c
Stanislav Koshynskyi <stasony10@gmail.com>
2026-03-14 10:31:39 +0200

    feat: email class

---
+++ src/main/java/entity/Email.java
@@ -0,0 +1,20 @@
+package entity;
+
+import lombok.Getter;
+
+@Getter
+public class Email {
+    private String email;
+    public void setEmail(String email) {
+        if (email == null) {
+            throw new NullPointerException("email is null");
+        }
+        if (!email.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,255}$")){
+            throw new IllegalArgumentException(email + " is not a valid email address");
+        }
+        this.email = email;
+    }
+    private Email(String email) {
+        setEmail(email);
+    }
+}
